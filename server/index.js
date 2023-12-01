20da874af5a86fb1ede35ff2bec01876618229ab
Priyanshu Chaudhary <chaudharypriyanshu1@gmail.com>
2023-12-02 01:05:41 +0530

    ssd

--- server/index.js
+++ server/index.js
@@ -15,13 +15,6 @@ const corsOptions = {
 
 require("dotenv").config();
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://chat-zone-app.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true'); // Allow credentials
-    next();
-});
 
 app.use(cors(corsOptions));
 app.use(express.json());
